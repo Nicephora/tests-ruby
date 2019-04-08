5f564e5ebb8c18c76543e1b7d18261338a2bddb5
nicephore <nicephorevictor@gmail.com>
2019-04-08 22:02:31 +0200

    Des modifs et ajouts  :)

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,4 +1,8 @@
-def time_string
-  
-
+def time_string(sec)
+Time.at(sec).utc.strftime("%H:%M:%S")
 end
+
+# autre méthode
+# def time_string(seconds)
+#   [seconds / 3600, seconds / 60 % 60, seconds % 60].map { |t| t.to_s.rjust(2,'0') }.join(':')
+# end
